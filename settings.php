0f1dcbc0d856a2fc720c1b9dbdabe1544ab6b0e2
Flottertotte <flottertotte@emeneo.com>
2015-05-07 13:10:49 +0800

    Revert "wrong submit"
    This reverts commit 2aabe6194c2363fa19ddf40fe8c162ce89aa278c.

--- settings.php
+++ settings.php
@@ -1,57 +1,25 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
- * *************************************************************************
- * *                  Apply	Enrol   				                      **
- * *************************************************************************
- * @copyright   emeneo.com                                                **
- * @link        emeneo.com                                                **
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
- * *************************************************************************
- * ************************************************************************
-*/
-defined('MOODLE_INTERNAL') || die();
+ * Resource module admin settings and defaults
+ *
+ * @package    mod_description
+ * @copyright  emeneo
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-if ($ADMIN->fulltree) {
-
-    //--- general settings -----------------------------------------------------------------------------------
-    $settings->add(new admin_setting_heading('enrol_apply_enrolname','',get_string('pluginname_desc', 'enrol_apply')));
-    
-    $settings->add(new admin_setting_configtext('enrol_apply/confirmmailsubject','',get_string('confirmmailsubject', 'enrol_apply'),null,PARAM_TEXT,60));
-
-    $settings->add(new admin_setting_heading('enrol_apply_confirmmailcontent', '', get_string('confirmmailcontent_desc', 'enrol_apply')));
-    $settings->add(new admin_setting_confightmleditor('enrol_apply/confirmmailcontent', get_string('confirmmailcontent', 'enrol_apply'),'utf-8',''));
-    
-    $settings->add(new admin_setting_configtext('enrol_apply/cancelmailsubject','',get_string('cancelmailsubject', 'enrol_apply'),null,PARAM_TEXT,60));
-
-    $settings->add(new admin_setting_heading('enrol_apply_cancelmailcontent', '', get_string('cancelmailcontent_desc', 'enrol_apply')));
-    $settings->add(new admin_setting_confightmleditor('enrol_apply/cancelmailcontent', get_string('cancelmailcontent', 'enrol_apply'),'utf-8',''));
-    
-	$settings->add(new admin_setting_configcheckbox('enrol_apply/sendmailtoteacher', get_string('sendmailtoteacher', 'enrol_apply'), '', 0));
-    $settings->add(new admin_setting_configcheckbox('enrol_apply/sendmailtomanager', get_string('sendmailtomanager', 'enrol_apply'), '', 0));
-    
-    //--- enrol instance defaults ----------------------------------------------------------------------------
-    $settings->add(new admin_setting_heading('enrol_manual_defaults',
-        get_string('enrolinstancedefaults', 'admin'), get_string('enrolinstancedefaults_desc', 'admin')));
-    
-    $settings->add(new admin_setting_configcheckbox('enrol_apply/defaultenrol',
-        get_string('defaultenrol', 'enrol'), get_string('defaultenrol_desc', 'enrol'), 0));
-    
-    $options = array(ENROL_INSTANCE_ENABLED => get_string('yes'),
-                     ENROL_INSTANCE_DISABLED  => get_string('no'));
-    $settings->add(new admin_setting_configselect('enrol_apply/status',
-        get_string('status', 'enrol_apply'), get_string('status_desc', 'enrol_apply'), ENROL_INSTANCE_ENABLED, $options));
-
-    if (!during_initial_install()) {
-        $options = get_default_enrol_roles(context_system::instance());
-        $student = get_archetype_roles('student');
-        $student = reset($student);
-        $settings->add(new admin_setting_configselect('enrol_apply/roleid',
-            get_string('defaultrole', 'role'), '', $student->id, $options));
-    }
-}
-
-if ($hassiteconfig) { // needs this condition or there is error on login page
-    $ADMIN->add('courses', new admin_externalpage('enrol_apply',
-            get_string('applymanage', 'enrol_apply'),
-            new moodle_url('/enrol/apply/manage.php')));
-}
+defined('MOODLE_INTERNAL') || die;
